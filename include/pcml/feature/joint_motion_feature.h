54994b32393241a140f806a23fdc7c30178e4750
jaesungp <jaesungp@cs.unc.edu>
2016-03-16 12:31:48 -0400

    created ros package

---
+++ include/pcml/feature/joint_motion_feature.h
@@ -0,0 +1,22 @@
+#ifndef PCML_JOINT_MOTION_FEATURE_H
+#define PCML_JOINT_MOTION_FEATURE_H
+
+
+#include <Eigen/Dense>
+
+
+namespace pcml
+{
+
+class JointMotionFeature
+{
+public:
+
+    JointMotionFeature();
+
+private:
+};
+
+}
+
+#endif // PCML_JOINT_MOTION_FEATURE_H
---
+++ src/feature/joint_motion_feature.cpp
@@ -0,0 +1,11 @@
+#include <pcml/feature/joint_motion_feature.h>
+
+
+namespace pcml
+{
+
+JointMotionFeature::JointMotionFeature()
+{
+}
+
+}
