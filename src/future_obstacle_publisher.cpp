24278169202c0b5854b731bf1940be60672e9b11
jaesungp <jaesungp@cs.unc.edu>
2016-05-02 14:25:13 -0400

    message defined and cmake setup

---
+++ src/future_obstacle_publisher.cpp
@@ -0,0 +1,12 @@
+#include <pcml/data/skeleton_stream.h>
+
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "future_obstacle_publisher");
+
+    ros::NodeHandle nh("~");
+
+    return 0;
+}
